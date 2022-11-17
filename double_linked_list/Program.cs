dfdc1e8ca3317e39b7314cbd88159df03c4980ea
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-11-17 16:48:44 +0700

    create class DoubleLinkedList & method addNode

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -13,8 +13,63 @@ namespace double_linked_list
         public Node next;
         public Node prev;
     }
+    class DoubleLinkedList
+    {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.rollNumber = rollNo;
+            newNode.name = nm;
 
-    internal class Program
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START != null)
+                    START.prev = newNode;
+                newNode.prev = null;
+                START = newNode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = START; current != null &&
+                rollNo >= current.rollNumber; previous = current, current =
+                current.next)
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            newNode.next = current;
+            newNode.prev = previous;
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            current.next = newNode;
+        }
+    }
+        internal class Program
     {
         static void Main(string[] args)
         {
